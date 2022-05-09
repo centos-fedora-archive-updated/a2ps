1c6a1f184a0ecf7759e868bd0a42a65cfb37af6d
Reuben Thomas <rrt@sc3d.org>
2022-05-10 00:51:33 +0100

    Add fix for CVE-2001-1593 (use of tempnam())

--- liba2ps/routines.h
+++ liba2ps/routines.h
@@ -242,7 +242,8 @@ FILE * xwpopen (const char * command);
 /* If _STR_ is not defined, give it a tempname in _TMPDIR_ */
 #define tempname_ensure(Str)				\
 do {							\
-  (Str) = (Str) ? (Str) : tempnam (NULL, "a2_");	\
+  (Str) = (Str) ? (Str) : safe_tempnam("a2_");	\
 } while (0)
+char * safe_tempnam(const char *);
 
 #endif
